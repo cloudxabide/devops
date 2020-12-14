603b0f409a0d69e685949e9e676737ed5bee36f2
James Radtke <cloudxabide@gmail.com>
2020-12-13 22:17:50 -0600

    Refactoring

--- Scripts/aws_cli_inventory.sh
+++ Scripts/aws_cli_inventory.sh
@@ -1,19 +1,5 @@
-# Inventory via AWS CLI
-This (ultimately) should provide the commands to inventory all the AWS resources we can query.
+#!/bin/sh
 
-STATUS:  Work in Progress
-         Executable without issue
-
-
-Rudimentary attempt at codifying a way to gather account inventory (in absence of AWS Config or Cost Explorer)
-
-* EC2
-* SecurityGroups
-* NACL
-* Routes
-
-## Non-Region specific objects
-```
 # IAM
 aws iam list-groups
 aws iam list-policies
@@ -21,10 +7,8 @@ aws iam list-policies
 ### Route53 Information
 echo "## S3 in $REGION"
 aws route53 list-hosted-zones
-```
 
 ## Region specific objects
-```
 header(){  echo "############################# ############################# #############################"; }
 footer(){  echo "############################# ############################# #############################"; }
 RED='\033[0;31m'
@@ -69,31 +53,7 @@ do
   echo "# CloudFormation Stacks in $REGION"
   aws cloudformation describe-stacks --query "Stacks[].[StackName,StackStatus]"
   footer
-
 done
-```
 
-### Other Example queries
-```
-aws ec2 describe-instances --query "Reservations[*].Instances[*].{name: Tags[?Key=='Name'] | [0].Value, IP: PublicIpAddress, ImageId: ImageId, TIme: LaunchTime}" --output table --color off
 
-aws ec2 describe-instances --query Reservations[].Instances[].[[Tags[*].Value],PublicIpAddress,ImageId,InstanceId,LaunchTime]
-
-# Search for the Red Hat CoreOS Images
-aws ec2 describe-images --owners 531415883065 --filters "Name=name,Values=rhcos-46*"  --query "sort_by(Images, &CreationDate)[*].[Name,ImageId]"
-```
-
-## NOTES
-## THIS STILL NEEDS WORK
-```
-aws ec2 describe-images --region $REGION  --filter "Name=is-public,Values=false" \
-    --query 'Images[].[ImageId, Name]' \
-    --output text | sort -k2
-
-## Show all AMI for a particular "owner" with "RHEL-8.2_HVM-*x86_64*" in the Name
-for REGION in $(aws ec2 describe-regions --query Regions[].RegionName --output text); do aws ec2 describe-images --owners  219670896067 --region $REGION --query 'sort_by(Images, &CreationDate)[*].[CreationDate,Name,ImageId]' --filters "Name=name,Values=RHEL-8.2_HVM-*x86_64*" --output text; done
-#variable "rhel8-ami" {
-#  type      = string
-#  default   = "ami-08e923f2f38197e46"
-#}
-```
+exit 0
