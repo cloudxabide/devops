541095efed675972dfe61c28e45dd4014cbd1f03
James Radtke <cloudxabide@gmail.com>
2026-03-30 20:04:42 -0400

    generic update

--- Scripts/10-OSX.sh
+++ Scripts/10-OSX.sh
@@ -139,7 +139,7 @@ brew install gnu-sed
 BINPATH="/opt/homebrew/Cellar/gnu-sed/4.9/libexec/gnubin/"
 grep $BINPATH ~/.bashrc.d/Darwin || { echo "PATH=\"$BINPATH:\$PATH\"" >> ~/.bashrc.d/Darwin; }
 
-PKGS="tree wget cmake jsonlint yamllint" 
+PKGS="tree wget cmake jsonlint yamllint asciidoctor" 
 for PKG in $PKGS; do brew install $PKG; done
 
 PKGS="nmap mcphost htop"
